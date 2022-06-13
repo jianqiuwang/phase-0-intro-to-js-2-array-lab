79558fd708bf13df576e2abb4e5572fda78c81fe
Jianqiu Wang <jianqiuwang1@icloud.com>
2022-06-13 16:03:17 -0500

    commit tree f944a7c922b9555345905aeafdf4e2ab206d8677

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    const name = cats.push('Ralph')
+    return name
+}
+
+function destructivelyPrependCat(){
+    const name = cats.unshift('Bob')
+    return name
+}
+
+function destructivelyRemoveLastCat(){
+    const name = cats.pop()
+    return name
+}
+
+const destructivelyRemoveFirstCat = () =>{
+    const name = cats.shift()
+    return name
+}
+
+function appendCat(){
+    const name = [...cats, 'Broom']
+    return name
+
+}
+
+function prependCat(){
+    const name = ['Arnold', ...cats]
+    return name
+}
+
+function removeLastCat(){
+    const name = cats.slice(0, length-1)
+    return name
+}
+
+function removeFirstCat(){
+    const name = cats.slice(1)
+    return name
+}
